e561cd078b648e5cc5fc6d42fd9fe9c9c242e1ce
Taegus <taegus.cromis@gmail.com>
2019-07-23 11:52:14 +0200

    use the new service API for pool data

--- js/pools.js
+++ js/pools.js
@@ -1,21 +1,4 @@
 var numberFormatter = new Intl.NumberFormat('en-US'); // US formatting, force commas.
-var poolList = [];
-
-var difficulties = [];
-var totalHashrate = 0;
-var totalMiners = 0;
-
-var poolsRefreshed = 0;
-
-var calculateTotalFee = function (config) {
-  let totalFee = config.config.fee;
-  for (let property in config.config.donation) {
-    if (config.config.donation.hasOwnProperty(property)) {
-      totalFee += config.config.donation[property];
-    }
-  }
-  return Math.round(totalFee * 1000) / 1000;
-};
 
 function localizeNumber(number) {
   return numberFormatter.format(number);
@@ -52,116 +35,30 @@ var renderPoolRow = function (label, host, name, data) {
   return pools_row.join('');
 };
 
-var translateAPI2 = function (data) {
-  return {
-    'network': {
-      'height': '',
-    },
-    'pool': {
-      'hashrate': data.pool_statistics.hashRate,
-      'miners': data.pool_statistics.miners,
-    },
-    'config': {
-      'minPaymentThreshold': ''
-    }
-  };
-};
+function getPoolName(data) {
+  var host = data.info.host;
+  var index = host.indexOf('/');
+  var poolName;
 
-$.getJSON('https://explorer.conceal.network/services/pools/list', function (data, textStatus, jqXHR) {
-  poolList = data;
-  console.log(poolList);
+  if (index < 0) {
+    return host;
+  } else {
+    return host.substr(0, index);
+  }
+}
 
-  poolList.forEach(function (element) {
-    var url = element[1];
-    var host = element[0];
-    var name = element[2];
-    var version = element[3];
-
-    if (version == "1") {
-      $.getJSON(url + '/stats', function (data, textStatus, jqXHR) {
-        var index = host.indexOf('/');
-        var poolName;
-
-        if (index < 0) {
-          poolName = host;
-        } else {
-          poolName = host.substr(0, index);
-        }
-
-        $('#pools_rows').append(renderPoolRow(name, host, poolName, data));
-      }).always(function () {
-        // do nothing
-      });
-    } else if (version == "2") {
-      var index = host.indexOf("/");
-      var poolName;
-
-      if (index < 0) {
-        poolName = host;
-      } else {
-        poolName = host.substr(0, index);
-      }
-
-      $.getJSON(url + '/pool/stats', function (data, textStatus, jqXHR) {
-        var tdata = translateAPI2(data);
-        $('#pools_rows').append(renderPoolRow(name, host, poolName, tdata));
-
-        $.getJSON(url + '/network/stats', function (data, textStatus, jqXHR) {
-          properHeight = data.height + 1;
-          updateText('height-' + poolName, localizeNumber(properHeight));
-        });
-      }).always(function () {
-        // do nothing
-      });
-    }
+$.getJSON('https://explorer.conceal.network/services/pools/data', function (data, textStatus, jqXHR) {
+  data.forEach(function (element) {
+    $('#pools_rows').append(renderPoolRow(element.info.name, element.info.host, getPoolName(element), element));
   });
 });
 
-
 setInterval(function () {
-
-  poolList.forEach(function (element) {
-    var url = element[1];
-    var host = element[0];
-    var name = element[2];
-    var version = element[3];
-
-    if (version == "1") {
-      var index = host.indexOf("/");
-      var poolName;
-      if (index < 0) {
-        poolName = host;
-      } else {
-        poolName = host.substr(0, index);
-      }
-
-      $.getJSON(url + '/stats', (data, textStatus, jqXHR) => {
-        updateText('height-' + poolName, localizeNumber(data.network.height));
-        updateText('hashrate-' + poolName, getReadableHashRateString(data.pool.hashrate));
-        updateText('miners-' + poolName, localizeNumber(data.pool.miners));
-      }).always(function () {
-        // do nothing
-      });
-    } else {
-      var index = host.indexOf("/");
-      var poolName;
-      if (index < 0) {
-        poolName = host;
-      } else {
-        poolName = host.substr(0, index);
-      }
-
-      $.getJSON(url + '/pool/stats', (data, textStatus, jqXHR) => {
-        updateText('hashrate-' + poolName, getReadableHashRateString(data.pool_statistics.hashRate));
-        updateText('miners-' + poolName, localizeNumber(data.pool_statistics.miners));
-      }).always(function () {
-        // do nothing
-      });
-
-      $.getJSON(url + '/network/stats', (data, textStatus, jqXHR) => {
-        updateText('height-' + poolName, localizeNumber(data.height));
-      });
-    }
+  $.getJSON('https://explorer.conceal.network/services/pools/data', function (data, textStatus, jqXHR) {
+    data.forEach(function (element) {
+      updateText('height-' + poolName, localizeNumber(element.network.height));
+      updateText('hashrate-' + poolName, getReadableHashRateString(element.pool.hashrate));
+      updateText('miners-' + poolName, localizeNumber(element.pool.miners));
+    });
   });
-
 }, 120000);
