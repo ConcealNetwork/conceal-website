01f5707bafc24fb0ab2750c8b0a7581f5a0d2628
taegus.cromis@gmail.com <nteGSt4N9cVRqhFb>
2019-08-21 12:43:44 +0200

    load pools only when scrolled into view

--- js/pools.js
+++ js/pools.js
@@ -1,4 +1,5 @@
 var numberFormatter = new Intl.NumberFormat('en-US'); // US formatting, force commas.
+var arePoolsLoaded = false;
 
 function localizeNumber(number) {
   return numberFormatter.format(number);
@@ -47,8 +48,25 @@ function getPoolName(data) {
   }
 }
 
-$.getJSON('https://explorer.conceal.network/services/pools/data', function (data, textStatus, jqXHR) {
-  data.forEach(function (element) {
-    $('#pools_rows').append(renderPoolRow(element.info.name, element.info.host, getPoolName(element), element));
-  });
+$(window).scroll(function () {
+  // This is then function used to detect if the element is scrolled into view
+  function elementScrolled(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    var elemTop = $(elem).offset().top;
+    return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
+  }
+
+  // This is where we use the function to detect if ".box2" is scrolled into view, and when it is add the class ".animated" to the <p> child element
+  if (elementScrolled('#mining')) {
+    if (!arePoolsLoaded) {
+      arePoolsLoaded = true;
+
+      $.getJSON('https://explorer.conceal.network/services/pools/data', function (data, textStatus, jqXHR) {
+        data.forEach(function (element) {
+          $('#pools_rows').append(renderPoolRow(element.info.name, element.info.host, getPoolName(element), element));
+        });
+      });
+    }
+  }
 });
