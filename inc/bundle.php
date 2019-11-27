c2944582d9d8c60b8e88b31fa6b9ca298aca79c7
Taegus <taegus.cromis@gmail.com>
2019-11-27 14:02:40 +0100

    create cache dir if it does not exist

--- inc/bundle.php
+++ inc/bundle.php
@@ -6,29 +6,29 @@ error_reporting(E_ERROR);
 // getlastmod() also provides this kind of timestamp for the last
 // modification date of the PHP file itself
 function cacheHeaders($lastModifiedDate) {
-    if ($lastModifiedDate) {
-        if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) >= $lastModifiedDate) {
-            if (php_sapi_name()=='CGI') {
-                Header("Status: 304 Not Modified");
-            } else {
-                Header("HTTP/1.0 304 Not Modified");
-            }
-            exit;
-        } else {
-            $gmtDate = gmdate("D, d M Y H:i:s \G\M\T",$lastModifiedDate);
-            header('Last-Modified: '.$gmtDate);
-        }
+  if ($lastModifiedDate) {
+    if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) >= $lastModifiedDate) {
+      if (php_sapi_name()=='CGI') {
+          Header("Status: 304 Not Modified");
+      } else {
+          Header("HTTP/1.0 304 Not Modified");
+      }
+      exit;
+    } else {
+      $gmtDate = gmdate("D, d M Y H:i:s \G\M\T",$lastModifiedDate);
+      header('Last-Modified: '.$gmtDate);
     }
+  }
 }
 
 // This function uses a static variable to track the most recent
 // last modification time
 function lastModificationTime($time=0) {
-    static $last_mod ;
-    if (!isset($last_mod) || $time > $last_mod) {
-        $last_mod = $time ;
-    }
-    return $last_mod ;
+  static $last_mod ;
+  if (!isset($last_mod) || $time > $last_mod) {
+    $last_mod = $time ;
+  }
+  return $last_mod ;
 }
 
 function joinPaths() {
@@ -43,6 +43,13 @@ function joinPaths() {
   return join('/', $paths);
 }
 
+// recursive create dir
+function mkpath($path)
+{
+  if(@mkdir($path) or file_exists($path)) return true;
+  return (mkpath(dirname($path)) and mkdir($path));
+}
+  
 lastModificationTime(filemtime(__FILE__));
 cacheHeaders(lastModificationTime());
 header("Content-type: " . $_GET['mime'] . "; charset: UTF-8");
@@ -51,26 +58,29 @@ ob_start ("ob_gzhandler");
 $cached_file = dirname(__DIR__) . '/cache/' . $_GET['build'];
 
 if (file_exists("$cached_file") === false) {
-    $cached_file_handle = fopen($cached_file, 'a+');
+  // make sure that directory exists
+  mkpath(dirname(__DIR__) . '/cache');
+  // create a new cache file in the cache directory
+  $cached_file_handle = fopen($cached_file, 'a+');
 
-    if ($cached_file_handle) {
-        // read the content of the file into the lines array
-        $lines = file($_GET['load'], FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+  if ($cached_file_handle) {
+    // read the content of the file into the lines array
+    $lines = file($_GET['load'], FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
-        foreach ($lines as $line_num => $value) {
-            $real_path = realpath(dirname(__DIR__) . '/' . $value);
-            
-            if (is_file("$real_path")) {
-                lastModificationTime(filemtime("$real_path"));
-                $content_file = file_get_contents("$real_path");
-                fwrite($cached_file_handle, $content_file);
-                fwrite($cached_file_handle, "\n");
-            }
-        }
-
-        // close the file handle
-        fclose($cached_file_handle);
+    foreach ($lines as $line_num => $value) {
+      $real_path = realpath(dirname(__DIR__) . '/' . $value);
+      
+      if (is_file("$real_path")) {
+        lastModificationTime(filemtime("$real_path"));
+        $content_file = file_get_contents("$real_path");
+        fwrite($cached_file_handle, $content_file);
+        fwrite($cached_file_handle, "\n");
+      }
     }
+
+    // close the file handle
+    fclose($cached_file_handle);
+  }
 }
 
 // returned cached file
