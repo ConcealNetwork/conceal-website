8c14a33a365a0d4a163d13abc56d2d567e73fed1
Alex Stanford <alex@leanagilist.dev>
2020-01-11 15:30:25 -0500

    Cleans up deposit calculator code

--- js/calc.js
+++ js/calc.js
@@ -1,5 +1,25 @@
 $(document).ready(function () {
 
+    function getTEA(nPrincipal,nMonths) {
+        let nBase;
+        let nEAR;
+        let nEIR;
+
+        if(nPrincipal<10000) {
+            nBase=0.029;
+        } else if(nPrincipal>=10000&&nPrincipal<20000) {
+            nBase=0.039;
+        } else if(nPrincipal>=20000) {
+            nBase=0.049;
+        }
+
+        nEAR=nBase+(nMonths-1)*0.001;
+
+        nEIR=nEAR/12*nMonths;
+
+        return nPrincipal * (1 + nEIR);
+    }
+
     $("#calc #calculate").click(function() {
         let nPrincipal;
         let nMonths;
@@ -8,26 +28,6 @@ $(document).ready(function () {
 
         nPrincipal=$('#nPrincipal').val();
         nMonths=$('#nMonths').val();
-    
-        function getTEA(nPrincipal,nMonths) {
-            let nBase;
-            let nEAR;
-            let nEIR;
-    
-            if(nPrincipal<10000) {
-                nBase=0.029;
-            } else if(nPrincipal>=10000&&nPrincipal<20000) {
-                nBase=0.039;
-            } else if(nPrincipal>=20000) {
-                nBase=0.049;
-            }
-    
-            nEAR=nBase+(nMonths-1)*0.001;
-    
-            nEIR=nEAR/12*nMonths;
-    
-            return nPrincipal * (1 + nEIR);
-        }
     
         nTEA=getTEA(nPrincipal,nMonths);
         nProfit=nTEA-nPrincipal;
