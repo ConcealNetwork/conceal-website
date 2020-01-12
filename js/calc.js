045add37c82901ffca210a72e14e7f37f295a162
Alex Stanford <alex@leanagilist.dev>
2020-01-11 20:59:34 -0500

    Simplifies calculator validation

--- js/calc.js
+++ js/calc.js
@@ -35,22 +35,16 @@ $(document).ready(function () {
         if(nPrincipal>9999999) {
             nPrincipal=9999999;
             $('#nPrincipal').val(nPrincipal);
-        } else if(nPrincipal<1&&nPrincipal!='') {
-            nPrincipal=1;
-            $('#nPrincipal').val(nPrincipal);
-        } else if(isNaN(nPrincipal)||typeof nPrincipal=='undefined'||nPrincipal=='') {
-            nPrincipal=0;
+        } else if(nPrincipal<1||isNaN(nPrincipal)||typeof nPrincipal=='undefined'||nPrincipal=='') {
+            nPrincipal='';
             $('#nPrincipal').val(nPrincipal);
         }
 
         if(nMonths>12) {
             nMonths=12;
             $('#nMonths').val(nMonths);
-        } else if(nMonths<1&&nMonths!='') {
-            nMonths=1;
-            $('#nMonths').val(nMonths);
-        } else if(isNaN(nMonths)||typeof nMonths=='undefined'||nMonths=='') {
-            nMonths=0;
+        } else if(nMonths<1||isNaN(nMonths)||typeof nMonths=='undefined'||nMonths=='') {
+            nMonths='';
             $('#nMonths').val(nMonths);
         }
     
@@ -70,13 +64,11 @@ $(document).ready(function () {
 
     $("#calc #nPrincipal")
         .keyup(runCalc)
-        .keypress(runCalc)
         .blur(runCalc)
         .change(runCalc);
 
     $("#calc #nMonths")
         .keyup(runCalc)
-        .keypress(runCalc)
         .blur(runCalc)
         .change(runCalc);
 
