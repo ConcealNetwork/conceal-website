b08becd1276116d87d8b65453934fbf1a50c442e
Alex Stanford <alex@leanagilist.dev>
2020-03-26 12:34:59 -0400

    Cleanup on language.js

--- js/language.js
+++ js/language.js
@@ -28,125 +28,101 @@ document.addEventListener("DOMContentLoaded", function(event) {
     
   }
 
-
-    var request = new XMLHttpRequest();
-
-
-    request.open('GET', 'inc/language.php', true);
-    request.onload = function() {
-      if (this.status >= 200 && this.status < 400) {
-        // Success!
-        var language = JSON.parse(this.response);
+  var request = new XMLHttpRequest();
 
 
-        request.open('GET', 'lang/selection.json', true);
-        request.onload = function() {
-          if (this.status >= 200 && this.status < 400) {
-            // Success!
-            var selection = JSON.parse(this.response);
+  request.open('GET', 'inc/language.php', true);
+  request.onload = function() {
+    if (this.status >= 200 && this.status < 400) {
+      // Success!
+      var language = JSON.parse(this.response);
 
-            for (var key in selection) {
-              if (selection.hasOwnProperty(key)) {
 
-                var newLangAnchor = document.createElement('a');
-                newLangAnchor.href = '#';
-                newLangAnchor.textContent = selection[key].name;
-                newLangAnchor.setAttribute('data-lang',key);
-                newLangAnchor.onclick = function() {
+      request.open('GET', 'lang/selection.json', true);
+      request.onload = function() {
+        if (this.status >= 200 && this.status < 400) {
+          // Success!
+          var selection = JSON.parse(this.response);
 
-                  var itemLangCode = this.getAttribute('data-lang');
-                  document.cookie = 'CCX_Language=' + itemLangCode + '; max-age=2629800; samesite=strict; secure';
+          for (var key in selection) {
+            if (selection.hasOwnProperty(key)) {
 
-                  request.open('GET', 'lang/' + itemLangCode + '.json', true);
-                  request.onload = function() {
-                    if (this.status >= 200 && this.status < 400) {
-                      // Success!
-                      var langData = JSON.parse(this.response);
+              var newLangAnchor = document.createElement('a');
+              newLangAnchor.href = '#';
+              newLangAnchor.textContent = selection[key].name;
+              newLangAnchor.setAttribute('data-lang',key);
+              newLangAnchor.onclick = function() {
 
-                      selectedLanguageEl = document.getElementById('selectedLanguage');
-                      selectedLanguageEl.textContent = selection[itemLangCode].name;
-                      langDropDown = document.getElementById('langDropdown');
-                      langDropDown.setAttribute('style','display:none;');
-                      translate(langData);
+                var itemLangCode = this.getAttribute('data-lang');
+                document.cookie = 'CCX_Language=' + itemLangCode + '; max-age=2629800; samesite=strict; secure';
 
-                    } else {
-                      // We reached our target server, but it returned an error
-                    }
-                  };
-                  request.onerror = function() {
-                    // There was a connection error of some sort
-                  };
-                  request.send();
+                request.open('GET', 'lang/' + itemLangCode + '.json', true);
+                request.onload = function() {
+                  if (this.status >= 200 && this.status < 400) {
+                    // Success!
+                    var langData = JSON.parse(this.response);
 
+                    selectedLanguageEl = document.getElementById('selectedLanguage');
+                    selectedLanguageEl.textContent = selection[itemLangCode].name;
+                    langDropDown = document.getElementById('langDropdown');
+                    langDropDown.setAttribute('style','display:none;');
+                    translate(langData);
+
+                  } else {
+                    // We reached our target server, but it returned an error
+                  }
                 };
+                request.onerror = function() {
+                  // There was a connection error of some sort
+                };
+                request.send();
 
-                langDropDown = document.getElementById('langDropdown');
-                langDropDown.appendChild(newLangAnchor);
+              };
 
-              }
+              langDropDown = document.getElementById('langDropdown');
+              langDropDown.appendChild(newLangAnchor);
+
             }
-
-
-            request.open('GET', 'lang/' + language + '.json', true);
-            request.onload = function() {
-              if (this.status >= 200 && this.status < 400) {
-                // Success!
-                var langData = JSON.parse(this.response);
-
-                document.getElementById('selectedLanguage').textContent = selection[language].name;
-                translate(langData);
-
-              } else {
-                // We reached our target server, but it returned an error
-              }
-            };
-            request.onerror = function() {
-              // There was a connection error of some sort
-            };
-            request.send();
-
-
-          } else {
-            // We reached our target server, but it returned an error
           }
-        };
-        request.onerror = function() {
-          // There was a connection error of some sort
-        };
-        request.send();
 
 
-      } else {
-        // We reached our target server, but it returned an error
-      }
-    };
+          request.open('GET', 'lang/' + language + '.json', true);
+          request.onload = function() {
+            if (this.status >= 200 && this.status < 400) {
+              // Success!
+              var langData = JSON.parse(this.response);
+
+              document.getElementById('selectedLanguage').textContent = selection[language].name;
+              translate(langData);
+
+            } else {
+              // We reached our target server, but it returned an error
+            }
+          };
+          request.onerror = function() {
+            // There was a connection error of some sort
+          };
+          request.send();
+
+
+        } else {
+          // We reached our target server, but it returned an error
+        }
+      };
+      request.onerror = function() {
+        // There was a connection error of some sort
+      };
+      request.send();
+
+
+    } else {
+      // We reached our target server, but it returned an error
+    }
+  };
   request.onerror = function() {
     // There was a connection error of some sort
   };
   request.send();
-
-  // Toggle element visibility
-  var toggle = function (elem) {
-    elem.classList.toggle('is-visible');
-  };
-
-  // Listen for click events
-  document.addEventListener('click', function (event) {
-
-    // Make sure clicked element is our toggle
-    if (!event.target.classList.contains('toggle')) return;
-
-    // Prevent default link behavior
-    event.preventDefault();
-
-    // Get the content
-    var content = document.querySelector(event.target.hash);
-    if (!content) return;
-
-    // Toggle the content
-    toggle(content);
-
-  }, false);
 
   document.onclick = function(event) {
     if (!event.target.closest('#langSelector')) {
