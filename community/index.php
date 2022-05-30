9bd9e0df20a8b8aec7afb97c509532bb0c0835c8
Overdrive <102536655+Overdrive-on@users.noreply.github.com>
2022-05-30 21:39:51 +0530

    revamped and updated community page

--- community/index.php
+++ community/index.php
@@ -2,6 +2,7 @@
 <html lang="en"><head>
   <title>Conceal - Community</title>
   <?php include '../landing/include/topjs.html';?>
+  <link rel="stylesheet" href="style/style.css">
 </head><body>
   <div id="loader-wrapper">
     <b></b>
@@ -81,104 +82,167 @@
         </div>
         <div class="row align-items-center speaker">
           <div class="col-lg-12 ml-auto communityWrapper">
-            <img data-aos="slide-up" src="../landing/images/community.png" />
-            <div class="pl-lg-5 communityLinks">
-              <div class="col">
-                <h2 class="text-uppercase text-primary d-block" data-aos="fade-up">
-                  Official
-                </h2>
-                <ul class="" data-aos="fade-up">
-                  <li><span class="icon-github"></span> <a href="https://github.com/ConcealNetwork">Github</a></li>
-                  <li><span class="icon-comments"></span> <a href="https://discord.gg/YbpHVSd">Discord</a></li>
-                  <li><span class="icon-telegram"></span> <a href="https://t.me/concealnetwork">Telegram</a></li>
-                  <li><span class="icon-twitter"></span> <a href="https://twitter.com/ConcealNetwork">Twitter</a></li>
-                  <li><span class="icon-facebook"></span> <a href="https://www.facebook.com/concealnetwork">Facebook</a></li>
-                  <li><span class="icon-medium"></span> <a href="https://medium.com/@ConcealNetwork">Medium</a></li>
-                  <li><span class="icon-reddit"></span> <a href="https://www.reddit.com/r/ConcealNetwork">Reddit</a></li>
-                  <li><span class="icon-btc"></span> <a href="https://bitcointalk.org/index.php?topic=4515873">BitcoinTalk</a></li>
-                </ul>
-              </div>
-              <div class="col">
-                <h2 class="text-uppercase text-primary d-block" data-aos="fade-up">
-                  Groups
-                </h2>
-                <h4>Telegram</h4>
-                <ul style="list-style:none;" data-aos="fade-up">
-                  <li><span class="icon-telegram"></span> <a href="https://t.me/concealnetworkusers">English</a></li>
-                  <li><span class="icon-telegram"></span> <a href="https://t.me/concealnetworkturkiye">T&uuml;rkiye</a></li>
-                  <li><span class="icon-telegram"></span> <a href="https://t.me/concealnetworkjapan">&#x65e5;&#x672c;&#x8a9e;</a></li>
-                  <li><span class="icon-telegram"></span> <a href="https://t.me/concealnetworkrussia">&#x420;&#x43e;&#x441;&#x441;&#x438;&#x44f;</a></li>
-                  <li><span class="icon-telegram"></span> <a href="https://t.me/concealnetworkdutch">Dutch</a></li>
-                  <li><span class="icon-telegram"></span> <a href="https://t.me/concealnetworkfrench">Fran&#xe7;ais</a></li>
-                  <li><span class="icon-telegram"></span> <a href="https://t.me/concealnetworkvietnam">Vi&#x1ec7;tnam</a></li>
-                  <li><span class="icon-telegram"></span> <a href="https://t.me/Conceal_Persian">Iran</a></li>
-                  <li><span class="icon-telegram"></span> <a href="https://t.me/Conceal_Bangladesh">Bangladesh</a></li>
-                  <li><span class="icon-telegram"></span> <a href="https://t.me/Conceal_India">India</a></li>
-                </ul>
-                <h4>Twitter</h4>
-                <ul style="list-style:none;" data-aos="fade-up">
-                  <li><span class="icon-twitter"></span> <a href="https://twitter.com/ConcealAfrica">Africa</a></li>
-                  <li><span class="icon-twitter"></span> <a href="https://twitter.com/ConcealSpanish">Espa&#xf1;ol</a></li>
-                  <li><span class="icon-twitter"></span> <a href="https://twitter.com/ConcealJapan">&#x65e5;&#x672c;&#x8a9e;</a></li>
-                </ul>
-              </div>
-              <div class="col">
-                <h2 id="exchanges" class="text-uppercase text-primary d-block" data-aos="fade-up">
-                  Exchanges
-                </h2>
-                <ul data-aos="fade-up">
-                  <li><a href="https://www.bitmart.com/trade/en?symbol=CCX_USDT&layout=basic">BitMart</a></li>
-                  <li><a href="https://app.stex.com/en/basic-trade/pair/BTC/CCX">STEX</a></li>
-                  <li><a href="https://www.hotbit.io/exchange?symbol=CCX_BTC">HotBit</a></li>
-                  <li><a href="https://tradeogre.com/exchange/BTC-CCX">TradeOgre</a></li>
-                </ul>
-              </div>
-              <div class="col">
-                <h2 id="Ethereum" class="text-uppercase text-primary d-block" data-aos="fade-up">
-                  Ethereum
-                </h2>
-                <ul data-aos="fade-up">
-                  <li><a href="https://1inch.exchange/#/r/0x9be82c0E5B75C53F32E63b40442E9dA8cCA06f21/ETH/wCCX">1Inch - ETH/wCCX</a></li>
-                  <li><a href="https://app.uniswap.org/#/swap?outputCurrency=0x21686f8ce003a95c99acd297e302faacf742f7d4">Uniswap - ETH/wCCX</a></li>
-                </ul>
-              </div>
-              <div class="col">
-                <h2 id="Binance" class="text-uppercase text-primary d-block" data-aos="fade-up">
-                  Binance Smart Chain
-                </h2>
-                <ul data-aos="fade-up">
-                  <li><a href="https://1inch.exchange/#/wCCX/WBNB">1Inch - wBNB/wCCX</a></li>
-                  <li><a href="https://1inch.exchange/#/wCCX/USDT">1Inch - USDT/wCCX</a></li>
-                  <li><a href="https://1inch.exchange/#/wCCX/BUSD">1Inch - BUSD/wCCX</a></li>
-                  <li><a href="https://1inch.exchange/#/wCCX/BNB">1Inch - BNB/wCCX</a></li>
-                  <li><a href="https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x988c11625472340b7b36ff1534893780e0d8d841">Pancakeswap - BNB/wCCX</a></li>
-                  <li><a href="https://exchange.pancakeswap.finance/#/swap?inputCurrency=0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c&outputCurrency=0x988c11625472340b7b36ff1534893780e0d8d841">Pancakeswap - wBNB/wCCX</a></li>
-                  <li><a href="https://exchange.pancakeswap.finance/#/swap?inputCurrency=0xe9e7cea3dedca5984780bafc599bd69add087d56&outputCurrency=0x988c11625472340b7b36ff1534893780e0d8d841">Pancakeswap - BUSD/wCCX</a></li>
-                  <li><a href="https://www.bakeryswap.org/#/swap?inputCurrency=0xe9e7cea3dedca5984780bafc599bd69add087d56&outputCurrency=0x988c11625472340b7b36ff1534893780e0d8d841">Bakeryswap - BUSD/wCCX</a></li>				  
-                  <li><a href=" https://app.sushi.com/swap?inputCurrency=0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56&outputCurrency=0x988c11625472340b7B36FF1534893780E0d8d841">Sushiswap - BUSD/wCCX</a></li>				  
-                </ul>
-              </div>
-              <div class="col">
-                <h2 id="Polygon" class="text-uppercase text-primary d-block" data-aos="fade-up">
-                  Polygon
-                </h2>
-                <ul data-aos="fade-up">
-                  <li><a href="https://app.sushi.com/swap#/swap?inputCurrency=0x137ee749f0f8c2ed34ca00de33bb59e3dafa494a&outputCurrency=0x2791bca1f2de4661ed88a30c99a7a9449aa84174">Sushiswap - USDC/wCCX</a></li>
-                  <li><a href="https://app.sushi.com/swap#/swap?inputCurrency=0x137ee749f0f8c2ed34ca00de33bb59e3dafa494a&outputCurrency=0xc2132d05d31c914a87c6611c10748aeb04b58e8f">Sushiswap - USDT/wCCX</a></li>
-                  <li><a href="https://app.sushi.com/swap#/swap?inputCurrency=0x137ee749f0f8c2ed34ca00de33bb59e3dafa494a&outputCurrency=0x7ceb23fd6bc0add59e62ac25578270cff1b9f619">Sushiswap - ETH/wCCX</a></li>
-                  <li><a href="https://app.sushi.com/swap#/swap?inputCurrency=0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270&outputCurrency=0x137ee749f0f8c2ed34ca00de33bb59e3dafa494a">Sushiswap - WMATIC/wCCX</a></li>
-                </ul>
-              </div>
-              <div class="col">
-                <h2 class="text-uppercase text-primary d-block" data-aos="fade-up">
-                  Market Cap
-                </h2>
-                <ul data-aos="fade-up">
-                  <li><a href="https://coinmarketcap.com/currencies/conceal">CoinMarketCap</a></li>
-                  <li><a href="https://www.coingecko.com/en/coins/conceal">CoinGecko</a></li>
-                </ul>
-              </div>
-            </div>
+            <img data-aos="slide-up" src="../landing/images/community.png" />			  
+			  <div class="navcontent" role="tabpanel" data-aos="fade-up" data-aos-delay="200">
+				<!-- Nav pills -->
+				<ul id="navBarTab" class="nav nav-tabs" role="tablist">
+					<li role="presentation" class="active">
+						<a data-target="#socials" href="#/" aria-controls="socials" role="tab" data-toggle="tab" class="active">Social links</a>
+					</li>|
+					<li role="presentation">
+						<a data-target="#exchanges" href="#/" aria-controls="exchanges" role="tab" data-toggle="tab">Exchanges</a>
+					</li>|
+					<li role="presentation">
+						<a data-target="#dex" href="#/" aria-controls="dex" role="tab" data-toggle="tab">DEX</a>
+					</li>
+				</ul>
+				
+			<!-- Tab panes -->
+    		<div class="tab-content">		
+
+				<div id="socials" role="tabpanel" class="tab-pane active"> 
+					<div class="row mb-5 aboutSection">
+					  <div class="col-lg-4" data-aos="fade-up" data-aos-delay="100">
+						<div class="site-section-heading">
+						  <h2>Social links</h2>
+						</div>
+					  </div>
+					</div>
+					<div class="row align-items-center speaker mediaSectionWrapper">
+						
+					  <div class="col slinks">
+						<h2 class="text-uppercase text-primary d-block" data-aos="fade-up">
+						  Official
+						</h2>
+						<ul data-aos="fade-up">
+						  <li><span class="icon-github"></span> <a href="https://github.com/ConcealNetwork">Github</a></li>
+						  <li><span class="icon-comments"></span> <a href="https://discord.gg/YbpHVSd">Discord</a></li>
+						  <li><span class="icon-telegram"></span> <a href="https://t.me/concealnetwork">Telegram</a></li>
+						  <li><span class="icon-twitter"></span> <a href="https://twitter.com/ConcealNetwork">Twitter</a></li>
+						  <li><span class="icon-youtube"></span> <a href="https://www.youtube.com/channel/UC_YtRUcy0FR0yIc3H6DDxuw">Youtube</a></li>
+						  <li><span class="icon-facebook"></span> <a href="https://www.facebook.com/concealnetwork">Facebook</a></li>
+						  <li><span class="icon-medium"></span> <a href="https://medium.com/@ConcealNetwork">Medium</a></li>
+						  <li><span class="icon-reddit"></span> <a href="https://www.reddit.com/r/ConcealNetwork">Reddit</a></li>
+						  <li><span class="icon-btc"></span> <a href="https://bitcointalk.org/index.php?topic=4515873">BitcoinTalk</a></li>
+						</ul>
+					  </div>
+
+					  <div class="col slinks">
+						<h2 class="text-primary d-block" data-aos="fade-up">
+						  Other Groups
+						</h2>
+						<h4 data-aos="fade-up">Telegram</h4>
+						<ul data-aos="fade-up">
+						  <li><span class="icon-telegram"></span> <a href="https://t.me/concealnetworkusers">English</a></li>
+						  <li><span class="icon-telegram"></span> <a href="https://t.me/concealnetworkturkiye">T&uuml;rkiye</a></li>
+						  <li><span class="icon-telegram"></span> <a href="https://t.me/concealnetworkrussia">&#x420;&#x43e;&#x441;&#x441;&#x438;&#x44f;</a></li>
+						  <li><span class="icon-telegram"></span> <a href="https://t.me/concealnetworkdutch">Dutch</a></li>
+						  <li><span class="icon-telegram"></span> <a href="https://t.me/concealnetworkfrench">Fran&#xe7;ais</a></li>
+						  <li><span class="icon-telegram"></span> <a href="https://t.me/concealnetworkvietnam">Vi&#x1ec7;tnam</a></li>
+						  <li><span class="icon-telegram"></span> <a href="https://t.me/Conceal_Persian">Iran</a></li>
+						  <li><span class="icon-telegram"></span> <a href="https://t.me/Conceal_Bangladesh">Bangladesh</a></li>
+						  <li><span class="icon-telegram"></span> <a href="https://t.me/Conceal_India">India</a></li>
+						</ul>
+						<h4 data-aos="fade-up">Twitter</h4>
+						<ul data-aos="fade-up">
+						  <li><span class="icon-twitter"></span> <a href="https://twitter.com/ConcealAfrica">Africa</a></li>
+						  <li><span class="icon-twitter"></span> <a href="https://twitter.com/ConcealSpanish">Espa&#xf1;ol</a></li>
+						  <li><span class="icon-twitter"></span> <a href="https://twitter.com/ConcealArabia">Arabia</a></li>
+						  <li><span class="icon-twitter"></span> <a href="https://twitter.com/concealturkiye">Türkiye</a></li>
+						</ul>
+					  </div>		
+					</div>
+				</div>
+
+				<div id="exchanges" role="tabpanel" class="tab-pane">
+					<div class="row mb-5 aboutSection">
+					  <div class="col-lg-4" data-aos="fade-up" data-aos-delay="100">
+						<div class="site-section-heading">
+						  <h2>Exchanges</h2>
+						</div>
+					  </div>
+					</div>
+					<div class="row align-items-center speaker mediaSectionWrapper">	
+						<div class="col slinks">
+							<h2 id="exchanges" class="text-uppercase text-primary d-block" data-aos="fade-up">
+							  Exchanges
+							</h2>
+							<ul data-aos="fade-up">
+							  <li><a href="https://www.bitmart.com/trade/en?symbol=CCX_USDT&layout=basic">BitMart</a></li>
+							  <li><a href="https://app.stex.com/en/basic-trade/pair/BTC/CCX">STEX</a></li>
+							  <li><a href="https://www.hotbit.io/exchange?symbol=CCX_BTC">HotBit</a></li>
+							  <li><a href="https://tradeogre.com/exchange/BTC-CCX">TradeOgre</a></li>
+							</ul>
+						</div>
+						
+						<div class="col slinks">
+							<h2 id="exchanges" class="text-uppercase text-primary d-block" data-aos="fade-up">
+							  Market Cap
+							</h2>
+							<ul data-aos="fade-up">
+							  <li><a href="https://coinmarketcap.com/currencies/conceal">CoinMarketCap</a></li>
+							  <li><a href="https://www.coingecko.com/en/coins/conceal">CoinGecko</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				
+				
+				<div id="dex" role="tabpanel" class="tab-pane">
+					<div class="row mb-5 aboutSection">
+					  <div class="col-lg-4" data-aos="fade-up" data-aos-delay="100">
+						<div class="site-section-heading">
+						  <h2>DEX</h2>
+						</div>
+					  </div>
+					</div>
+					<div class="row align-items-center speaker mediaSectionWrapper">
+						<div class="col slinks">
+							<h2 id="exchanges" class="text-uppercase text-primary d-block" data-aos="fade-up">
+							  Polygon
+							</h2>
+							<ul data-aos="fade-up">
+							  <li><a href="https://app.sushi.com/swap?tokens=0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174&tokens=0x137Ee749f0F8c2eD34cA00dE33BB59E3dafA494A&chainId=137">SushiSwap - wCCX/USDC</a></li>
+							  <li><a href="https://app.sushi.com/swap?tokens=0xc2132D05D31c914a87C6611C10748AEb04B58e8F&tokens=0x137Ee749f0F8c2eD34cA00dE33BB59E3dafA494A&chainId=137">SushiSwap - wCCX/USDT</a></li>
+							  <li><a href="https://app.uniswap.org/#/swap?inputCurrency=0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063&outputCurrency=0x137ee749f0f8c2ed34ca00de33bb59e3dafa494a&chain=polygon">
+								Uniswap - wCCX/DAI </a>
+							  </li>
+							  <li><a href="https://app.uniswap.org/#/swap?inputCurrency=0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174&outputCurrency=0x137ee749f0f8c2ed34ca00de33bb59e3dafa494a&chain=polygon">
+								Uniswap - wCCX/USDC</a>
+							  </li>
+							  <li><a href="https://app.uniswap.org/#/swap?inputCurrency=0xc2132D05D31c914a87C6611C10748AEb04B58e8F&outputCurrency=0x137ee749f0f8c2ed34ca00de33bb59e3dafa494a&chain=polygon">
+								Uniswap - wCCX/USDT</a>
+							  </li>
+							</ul>
+						</div>
+						<div class="col slinks">
+							<h2 id="exchanges" class="text-uppercase text-primary d-block" data-aos="fade-up">
+							  Binance Smart Chain
+							</h2>
+							<ul data-aos="fade-up">
+							  <li><a href="https://pancakeswap.finance/swap?inputCurrency=0xe9e7cea3dedca5984780bafc599bd69add087d56&outputCurrency=0x988c11625472340b7b36ff1534893780e0d8d841">
+								  PancakeSwap - wCCX/BUSD</a>
+							  </li>
+							  <li><a href="https://pancakeswap.finance/swap?inputCurrency=0x55d398326f99059fF775485246999027B3197955&outputCurrency=0x988c11625472340b7b36ff1534893780e0d8d841">
+								  PancakeSwap - wCCX/USDT</a>
+							  </li>
+							</ul>
+						</div>
+						<div class="col slinks">
+							<h2 id="exchanges" class="text-uppercase text-primary d-block" data-aos="fade-up">
+							  Etherium
+							</h2>
+							<ul data-aos="fade-up">
+							  <li><a href="https://app.uniswap.org/#/swap?inputCurrency=0x6b175474e89094c44da98b954eedeac495271d0f&outputCurrency=0x21686f8ce003a95c99acd297e302faacf742f7d4&chain=mainnet">
+								  Uniswap - wCCX/DAI</a>
+							  </li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				
+				</div>
+			  </div>
           </div>
         </div>
       </div><!-- .cointainer -->
