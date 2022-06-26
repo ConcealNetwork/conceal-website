f8fdf9f99b5c704267188a17d80c9fb7b7412e4a
Overdrive <102536655+Overdrive-on@users.noreply.github.com>
2022-06-26 18:48:59 +0530

    New Team Page

---
+++ team/index.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+  <title>Conceal - Team</title>
+  <?php include '../landing/include/topjs.html';?>
+  <link rel="stylesheet" href="style/style.css">
+</head><body>
+  <div id="loader-wrapper">
+    <b></b>
+    <div id="loader"></div>
+  </div>
+  <div class="site-wrap">
+    <div class="site-mobile-menu">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+    <header class="site-navbar py-3" role="banner" id="siteHeader">
+      <div class="container-fluid">
+        <div class="row align-items-center">
+          <div class="col-12 col-md-1" id="logoDiv">
+            <a href="../"></a>
+          </div>
+          <div class="col-12 col-md-11 d-none d-xl-block">
+            <nav class="site-navigation position-relative text-right" role="navigation">
+              <ul class="site-menu js-clone-nav mx-auto d-none d-lg-block">
+                <li><a href="../banking/">BANKING</a></li>
+                <li><a href="https://conceal.cloud">CLOUD</a></li>
+                <li><a href="#">ID</a></li>
+                <li><a href="../labs/">LABS</a></li>
+                <li><a href="../messaging/">MESSAGING</a></li>
+                <li><a href="../mobile/">MOBILE</a></li>
+				<li><a href="../clive/">Clive</a></li>
+				<li><a href="../pay/">PAY</a></li>
+                <li><a href="../app/">Conceal App</a></li>
+              </ul>
+            </nav>
+          </div>
+		  <div class="header-language">
+			<a id="langSelector" href="#">
+			  <i class="fas fa-language fa-2x align-middle"></i>
+			    <span id="selectedLanguage">English</span>
+  		    </a>
+			<div id="langDropdown" class="dropdown-content"></div>
+		  </div>		  		  		  
+          <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3">
+            <a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="site-section site-hero">
+      <div class="container">
+		<div class="row align-items-center">
+          <div class="col-md-10">
+            <span class="d-block mb-3 caption" data-aos="fade-up" data-aos-delay="100">CONCEAL TEAM</span>
+            <h1 class="d-block mb-4" data-aos="fade-up" data-aos-delay="200">
+				<strong>Alone</strong> we can do so little; <strong> together </strong>we can do so much.
+            </h1>
+            <a href="#downArrowBtn" class="btn-custom btnSignMain" data-aos="fade-up" data-aos-delay="400"><span class="icon-leanpub"></span><span>Click Here To Meet The Team</span></a>
+          </div>
+        </div>  	  
+        <div class="downArrowWrapper">
+          <a href="#downArrowBtn" class="downArrow" id="downArrowBtn"></a>
+        </div>
+      </div>
+    </div>
+	  
+	  
+    <div class="site-section" id="mainSection">
+	  <div class="container">
+		<div class="section-title" data-aos="fade-up" data-aos-delay="100">
+            <h1>Meet the team</h1>
+        </div>  	
+	  
+		 <div class="row justify-content-around pb-5">
+		 	<div class="column pb-5" data-aos="fade-up" data-aos-delay="400">
+				<div class="team">
+					<div class="team-img">
+						<img src="../images/media/media_articles_02.png" alt="">
+					</div>
+					<h3 class="team-name">krypt0x (aka 0x)</h3>
+					<span class="role">HEAD OF OPERATIONS</span>
+					<ul class="socials">
+						<li><a href="https://github.com/krypt0x" class="fab fa-github" target="_blank" rel="noopener noreferrer"></a></li>
+						<li><a href="https://twitter.com/Krypt0xChaos" class="fab fa-twitter" target="_blank" rel="noopener noreferrer"></a></li>
+					</ul>
+					<div class="tdesc"><p class="desc">With great power, comes great responsibility</p></div>
+				</div>
+			</div>
+			 
+			<div class="column pb-5" data-aos="fade-up" data-aos-delay="400">
+				<div class="team">
+					<div class="team-img">
+						<img src="../images/media/media_articles_02.png" alt="">
+					</div>
+					<h3 class="team-name">Taegus</h3>
+					<span class="role">Head of Development</span>
+					<ul class="socials">
+						<li><a href="https://github.com/taeguscromis" class="fab fa-github" target="_blank" rel="noopener noreferrer"></a></li>
+						<li><a href="https://twitter.com/Taegus1" class="fab fa-twitter" target="_blank" rel="noopener noreferrer"></a></li>
+					</ul>
+					<div class="tdesc"><p class="desc">With great power, comes great responsibility</p></div>
+				</div>
+			</div>
+			 
+			<div class="column pb-5" data-aos="fade-up" data-aos-delay="400">
+				<div class="team">
+					<div class="team-img">
+						<img src="../images/media/media_articles_02.png" alt="">
+					</div>
+					<h3 class="team-name">okanist (aka Okan)</h3>
+					<span class="role">HEAD OF Business Development</span>
+					<ul class="socials">
+						<li><a href="https://twitter.com/okansariirmak" class="fab fa-twitter" target="_blank" rel="noopener noreferrer"></a></li>
+					</ul>
+					<div class="tdesc"><p class="desc">With great power, comes great responsibility</p></div>
+				</div>
+			</div> 
+			 		 
+			<div class="column pb-5" data-aos="fade-up" data-aos-delay="400">
+				<div class="team">
+					<div class="team-img">
+						<img src="../images/media/media_articles_02.png" alt="">
+					</div>
+					<h3 class="team-name">Mr_Kwibs</h3>
+					<span class="role">HEAD OF Marketing</span>
+					<ul class="socials">
+						<li><a href="https://twitter.com/Mr_Kwibs" class="fab fa-twitter" target="_blank" rel="noopener noreferrer"></a></li>
+					</ul>
+					<div class="tdesc"><p class="desc">With great power, comes great responsibility</p></div>
+				</div>
+			</div> 
+			 		 
+			<div class="column pb-5" data-aos="fade-up" data-aos-delay="400">
+				<div class="team">
+					<div class="team-img">
+						<img src="../images/teampage/LolitaLollipop.png" alt="">
+					</div>
+					<h3 class="team-name">LolitaLollipop</h3>
+					<span class="role">HEAD OF Community</span>
+					<ul class="socials">
+						<li><a href="https://twitter.com/CryptoLolli" class="fab fa-twitter" target="_blank" rel="noopener noreferrer"></a></li>
+					</ul>
+					<div class="tdesc"><p class="desc">A lover of Freedom and Privacy, interested in Cryptography, and perfectly Anonymous. Discovering Conceal in early 2019 I was blown away by the Vision and 	the Adherence to the Core Principles found in the Satoshi Nakamoto whitepaper, and I've been here ever since. I'm on a personal mission to see Privacy become a normal part of everyone's 	    daily life!</p>
+					</div>
+				</div>
+			</div> 
+			 
+			<div class="column pb-5" data-aos="fade-up" data-aos-delay="400">
+				<div class="team">
+					<div class="team-img">
+						<img src="../images/media/media_articles_02.png" alt="">
+					</div>
+					<h3 class="team-name">ZeMaNel</h3>
+					<span class="role">Cloud Operations</span>
+					<ul class="socials">
+						<li><a href="https://github.com/cryptomanel" class="fab fa-github" target="_blank" rel="noopener noreferrer"></a></li>
+					</ul>
+					<div class="tdesc"><p class="desc">With great power, comes great responsibility</p></div>
+				</div>
+			</div> 
+			 
+			<div class="column pb-5" data-aos="fade-up" data-aos-delay="400">
+				<div class="team">
+					<div class="team-img">
+						<img src="../images/media/media_articles_02.png" alt="">
+					</div>
+					<h3 class="team-name">bomb-on</h3>
+					<span class="role">Developer</span>
+					<ul class="socials">
+						<li><a href="https://github.com/bomb-on" class="fab fa-github" target="_blank" rel="noopener noreferrer"></a></li>
+					</ul>
+					<div class="tdesc"><p class="desc">With great power, comes great responsibility</p></div>
+				</div>
+			</div> 
+			 
+			<div class="column pb-5" data-aos="fade-up" data-aos-delay="400">
+				<div class="team">
+					<div class="team-img">
+						<img src="../images/media/media_articles_02.png" alt="">
+					</div>
+					<h3 class="team-name">ThrownLemon</h3>
+					<span class="role">Developer</span>
+					<ul class="socials">
+						<li><a href="https://github.com/ThrownLemon" class="fab fa-github" target="_blank" rel="noopener noreferrer"></a></li>
+					</ul>
+					<div class="tdesc"><p class="desc">With great power, comes great responsibility</p></div>
+				</div>
+			</div>  
+			 
+			<div class="column pb-5" data-aos="fade-up" data-aos-delay="400">
+				<div class="team">
+					<div class="team-img">
+						<img src="../images/media/media_articles_02.png" alt="">
+					</div>
+					<h3 class="team-name">AxVultis</h3>
+					<span class="role">Developer</span>
+					<ul class="socials">
+						<li><a href="https://github.com/AxVultis" class="fab fa-github" target="_blank" rel="noopener noreferrer"></a></li>
+					</ul>
+					<div class="tdesc"><p class="desc">With great power, comes great responsibility</p></div>
+				</div>
+			</div>  
+			 
+			<div class="column pb-5" data-aos="fade-up" data-aos-delay="400">
+				<div class="team">
+					<div class="team-img">
+						<img src="../images/media/media_articles_02.png" alt="">
+					</div>
+					<h3 class="team-name">CartoonFace</h3>
+					<span class="role">Developer</span>
+					<ul class="socials">
+						<li><a href="https://github.com/cartoon-face" class="fab fa-github" target="_blank" rel="noopener noreferrer"></a></li>
+						<li><a href="https://twitter.com/CartoonFace_Dev" class="fab fa-twitter" target="_blank" rel="noopener noreferrer"></a></li>
+					</ul>
+					<div class="tdesc"><p class="desc">With great power, comes great responsibility</p></div>
+				</div>
+			</div> 
+			 
+			<div class="column pb-5" data-aos="fade-up" data-aos-delay="400">
+				<div class="team">
+					<div class="team-img">
+						<img src="../images/teampage/Mannan.png" alt="">
+					</div>
+					<h3 class="team-name">Mannan</h3>
+					<span class="role">Community Manager</span>
+					<ul class="socials">
+						<li><a href="https://twitter.com/CryptolordTG" class="fab fa-twitter" target="_blank" rel="noopener noreferrer"></a></li>
+					</ul>
+					<div class="tdesc"><p class="desc">I began my crypto journey in mid-2017 and discovered Conceal in late 2020. In September 2021, I was promoted to join the core team to help our project grow. I've learned a lot over the years, and I'm still on a journey of learning and being the best I can be.</p></div>
+				</div>
+			</div>  
+			 
+			<div class="column pb-5" data-aos="fade-up" data-aos-delay="400">
+				<div class="team">
+					<div class="team-img">
+						<img src="../images/media/media_articles_02.png" alt="">
+					</div>
+					<h3 class="team-name">CryptTrader</h3>
+					<span class="role">Marketing</span>
+					<ul class="socials">
+						<li><a href="https://twitter.com/Cryp_Tader" class="fab fa-twitter" target="_blank" rel="noopener noreferrer"></a></li>
+					</ul>
+					<div class="tdesc"><p class="desc">With great power, comes great responsibility</p></div>
+				</div>
+			</div>  
+			 	 
+		</div>
+			 
+			  
+		 
+		<div class="row mb-5 aboutSection">
+          <div class="col-lg-4" data-aos="fade-up" data-aos-delay="100">
+            <div class="site-section-heading">
+              <h2>About</h2>
+            </div>
+          </div>
+          <div class="col-lg-8 mt-8 pl-lg-8" data-aos="fade-up" data-aos-delay="200">
+            <p>
+              The Conceal Team is a group of dedicated Volunteers from the Community who have committed time and energy into managing the Development, Marketing, and Community Building efforts of the Conceal Network.
+            </p>			  
+            <p>
+			  Conceal Team members operate as a DAO, or Decentralized Autonomous Organization, and each member will take on tasks that He or She can specialize in, voluntarily and without the presence of any third party or legal body. In order to further organize the day to day operations of our DAO, the Team has created Divisions with separate responsibilities so that members can further specialize in the overall development and growth of the network.
+            </p>
+			<p>
+			  Conceal Team members are from all four corners of the world and are spread out around many varied geographical locations! This means that organizing events and other types of online gatherings can be tricky, but not impossible. Anyone can join as long as they are willing to commit more time into the Conceal ecosystem and have the necessary skill to help make it succeed!
+            </p>    
+          </div>
+        </div>  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+      </div><!-- .cointainer -->
+    </div><!-- #mainSection -->
+   <?php include '../landing/include/footer.html';?>
+  </div>
+  <?php include '../landing/include/bottomjs.html';?>
+</body></html>
