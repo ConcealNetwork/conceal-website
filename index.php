238e86f62acce5e986c49d7dd0f15669159cbbed
Øx <35565671+krypt0x@users.noreply.github.com>
2022-12-14 14:30:05 +0000

    typo in the menu
    rename bridge

--- index.php
+++ index.php
@@ -74,7 +74,7 @@ echo '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8">
                         <label for="toggle_submenusOff"></label>
                         <ul>
                             <li><a href="app">Conceal-App</a></li>
-                            <li><a href="https://bridge.conceal.network">Bridge</a></li>
+                            <li><a href="https://bridge.conceal.network">Conceal-Bridge</a></li>
 							<li><a href="defi/">Conceal-DeFi</a></li>
                             <li><a href="#wallets">Conceal-Desktop</a></li>
 							<li><a href="features">Conceal-Earn</a></li>
